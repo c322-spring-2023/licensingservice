3a5b2c5b19124566f0bcb87fcc1d27cdf6954648
Hamidreza Bahramian <hamid.bahramian@gmail.com>
2023-04-22 22:13:50 -0700

    circuit breaker pattern added.

--- src/main/java/edu/iu/c322/assetmanagement/licensingservice/LicensingserviceApplication.java
+++ src/main/java/edu/iu/c322/assetmanagement/licensingservice/LicensingserviceApplication.java
@@ -3,10 +3,12 @@ package edu.iu.c322.assetmanagement.licensingservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@RefreshScope
 public class LicensingserviceApplication {
 
 	public static void main(String[] args) {
